c8373384f0680d6087612bce0a3a3e26d750a328
蕉灼の仓鼠 <49329079+jzcangshu@users.noreply.github.com>
2023-03-12 14:26:56 +0800

    修改main.py为用户程序，删除后端爬取功能

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-#此程序为主程序
+# -- coding: utf-8 --**
+#此程序为参与抽奖主程序
 import configparser
 import requests
 import json
 
-#下方区域放置所有函数备用
+#——————————下方区域放置所有函数备用——————————#
 'cookie_seperator函数用于格式化从config.ini中读取到的CK变量备用 【注意】cookie中只应包含值 不要含有中文！'
 def cookie_seperator(cookie): 
     cookies = {}
@@ -13,18 +14,13 @@ def cookie_seperator(cookie):
         tmp_index=i[:i.find('=')]
         cookies[tmp_index]=temp
     return cookies
-def spider():
-    print(pages_cnt)
-
-#下方区域为初始化变量
+#——————————下方区域为初始化变量——————————#
 '读取配置项'
 cfp = configparser.RawConfigParser()
 cfp.read("config.ini")
 '读取CK'
 cookie_input = cfp.get("Cookies", "ck")
 cookies = cookie_seperator(cookie_input)
-'读取爬取的文章页数'
-set_pages_cnt=int(cfp.get("Spider", "pages_cnt"))
 headers = {
     'Accept': 'application/json, textain, */*',
     'Accept-Language': 'zh-CN,zh;q=0.9',
@@ -49,12 +45,7 @@ data = {
     'offset': '',
     'tagIds[0]': 'new',
 }
-pages_cnt = 1
 
-#下方开始主程序
-print('——————开始获取',set_pages_cnt,'页情报——————')
-while pages_cnt <= set_pages_cnt:
-    response = requests.post('https://www.zfrontier.com/v2/home/flow/list', cookies=cookies, headers=headers, data=data,verify=False).json()
-    data['offset'] = response['data']['offset']
-    print(data['offset'])
-    pages_cnt += 1
+
+#——————————下方开始主程序——————————#
+
