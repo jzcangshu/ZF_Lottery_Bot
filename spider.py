01833ec6108322d0bdfa4c58655734c0b9f848e7
蕉灼の仓鼠 <49329079+jzcangshu@users.noreply.github.com>
2023-04-01 22:01:44 +0800

    在抽奖信息中增加群昵称信息 适当增加代码注释

--- spider.py
+++ spider.py
@@ -14,18 +14,20 @@ def spider(r,article_cnt):
         if len(json_str) == 0:            #如果文件为空则需要初始化JSON格式
             json_str = '[]'
         data_list = json.loads(json_str)  # 将JSON数据解析为Python列表
-        for article in r['data']['list']: # 开始获取单个帖子详情
 
+
+        for article in r['data']['list']: # 开始获取单个帖子详情
             temp_lottery_info_dict = {}   # 存储单个帖子中的抽奖信息
             temp_lottery_info_dict['id'] = article['id']
             temp_lottery_info_dict['hash_id'] = article['hash_id']
             view_url = 'https://www.zfrontier.com/v2/flow/detail/' + article['hash_id']
 
+            #进行去重过滤
             if str(article['id']) in json_str: #如果该抽奖已被存储过则跳过
                 article_cnt -= 1
                 continue
 
-            cnt = 0
+            cnt = 0 #重试请求次数计数器
             view_headers['Referer'] = view_url
             response = requests.post(view_url, proxies=proxies,cookies=cookies, headers=view_headers, data=data, verify=False).json() # 获取详情
             while cnt<= 3 and ( response['msg'] == '操作太频繁了' or response['data'] == []):
@@ -43,7 +45,7 @@ def spider(r,article_cnt):
             if response['data']['flow']['lottery'] != None: #如果抽奖信息非空
                 if response['data']['flow']['lottery']['status_str'] == '待抽奖':
                     temp_lottery_info_dict['lottery_time'] = response['data']['flow']['lottery']['lottery_at'] #开奖时间 格式-> '2023-03-31 20:20'
-                    temp_lottery_info_dict['lottery_qq'] = response['data']['flow']['plate']['qq'] #str格式的抽奖群号
+                    temp_lottery_info_dict['lottery_qq'] = response['data']['flow']['plate']['name'] + ' ' + response['data']['flow']['plate']['qq']  #str格式的抽奖群号+群昵称
                     temp_lottery_info_dict['jq_flag'] = 'F' #初始化变量“是否需要加群”
                     for awards in response['data']['flow']['lottery']['prizesGroup']: #jq_flag存储是否需要加群领奖（值为T或F）
                         if '群' in awards['name']: #奖品名称中写明需要加群领奖
@@ -51,7 +53,7 @@ def spider(r,article_cnt):
                 data_list.append(temp_lottery_info_dict) # 将新的JSON数据添加到Python对象中（单个抽奖信息）
             time.sleep(15)
         json_str = json.dumps(data_list)         # 将Python对象转换为JSON格式的字符串
-        f.write(json_str)                        # 将被筛选出的抽奖帖子信息写入到.json文件中
+        f.write(json_str)                        # 将被筛选出的抽奖帖子信息写回到.json文件中(包括原有的)
 
 'cookie_seperator函数用于格式化从config.ini中读取到的CK变量备用 【注意】cookie中只应包含值 不要含有中文！'
 def cookie_seperator(cookie): 
@@ -123,8 +125,8 @@ data = {
     'offset': '',
     'tagIds[0]': '2007',
 }
-pages_cnt = 1
-article_cnt = -1 #未定义
+pages_cnt = 1    #获取帖子列表页数
+article_cnt = -1 #未定义 在spider程序中用于统计这一页获取到的帖子数量
 
 
 #——————————下方开始主程序——————————#
